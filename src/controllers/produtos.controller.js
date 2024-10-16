0012089b83b1c12a191987f3770cf481bb8a0ff3
artuur <arturcastrodossantos.com@gmail.com>
2024-10-16 19:18:57 -0300

    controller de produto metodo create refeito para melhoria de codigo, codigo agora está retornado as respstas http corretas

--- src/controllers/produtos.controller.js
+++ src/controllers/produtos.controller.js
@@ -1,67 +1,28 @@
-import express from 'express'
-export const router = express.Router()
-import { ProdutoService } from '../services/produtos.services.js'
-import { PrismaClient } from '@prisma/client'
-const produtoService = new ProdutoService()
-const prisma = new PrismaClient()
+import express from 'express';
+const router = express.Router();
+import { ProdutoServices } from '../services/produtos.services.js';
+
+const produtoServices = new ProdutoServices();
 
 router.post('/', async (req, res) => {
+    const data = req.body;
+
+
+    if (!data.nome || !data.localizacaoNoDeposito || !data.quantidadeEmEstoque || !data.preco || !data.categoria) {
+        return res.status(400).json({ error: 'Todos os campos são necessários' });
+    }
 
     try {
-
-    const data = req.body
-    
-    if (!data.nome || !data.categoria || !data.preco || !data.quantidadeEmEstoque || !data.localizacaoNoDeposito) {
-
-        return res.status(400).json({error: 'todos os campos são obrigatorios'})
+        
+        const produtos = await produtoServices.create(data);
+        return res.json(produtos);
+    } 
+    catch (error) {
+        
+        const status = error.status || 500;
+        const message = error.details || 'Erro interno do servidor';
+        return res.status(status).json({ message });
     }
+});
 
-    const produto = await produtoService.create(data)
-
-    res.status(201).json(produto)
-
-    }
-    catch(err) {
-        console.log(err)
-        return res.status(500).json({error: 'não foi possivel criar o produto'})
-    }
-
-})
-
-
-router.get('/', async (req, res) => {
-
-    try {
-        const produtos = await produtoService.read()
-        return res.status(200).json(produtos)
-
-    }
-    catch(err) {
-        console.log(err)
-        return res.status(500).json('erro')
-    }
-
-
-})
-
-
-router.get('/:id', async (req, res) => {
-
-    try {
-        const id = parseInt(req.params.id, 10)
-
-        if (!id) {
-            return res.status(400).json({error: 'id do produto não foi informado corretamente'})
-        }
-
-        const produto = await produtoService.showProduto(id)
-
-        res.status(200).json(produto)
-
-    }
-    catch(err) {
-        console.log(err)
-        return res.status(500).json({error: 'erro ao buscar'})
-    }
-
-})
+export default router;
